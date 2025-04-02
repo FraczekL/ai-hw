34a933c3f48d0b1a06a0e189f27ff0b1f67b8a39
Luke Fraczek <FraczekL@gmail.com>
2025-04-01 19:24:18 -0500

    initial commit

---
+++ high-risk-project/high-risk-project.py
@@ -0,0 +1,8 @@
+import os
+import numpy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
